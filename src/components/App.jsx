0d2a37968c55896aa9d498356f9ef9748161709a
Fredy Piraquive <40009527+Judiu@users.noreply.github.com>
2024-03-19 08:40:09 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { jsPDF } from 'jspdf';
 
+const fecha = new Date().toLocaleDateString();
+
 function App() {
   //
   const generatePDF = () => {
@@ -9,12 +11,28 @@ function App() {
   }
   const renderPDF = () => {
     const doc = new jsPDF();
-    doc.addImage('./Ferroscan.jpg', 'JPG', 0, 0, 210, 297);
-    doc.text('Hello world!', 35, 25);
-    // Crean el PDF de formato para poder de notaria
+    doc.setFontSize(12);
+    var fecha = document.getElementById('fecha').value;
+    var fechaObj = new Date(fecha + 'T00:00'); // Asegura que la fecha se interpreta en horario local
+    var fechaLarga = fechaObj.toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    var ciudad = document.getElementById('ciudad').value;
+    if (ciudad === 'otra') {
+      ciudad = prompt('Ingrese la ciudad');
+    }
+    var curaduria = document.getElementById('curaduria').value;
+    if (ciudad === 'Mosquera' && curaduria === 'dos') {
+      var profesional = 'Abogada';
+      var nompro = 'MARIA CRISTINA ARENAS GUEVARA';
+      var cur = 'CURADURIA URBANA No. 2';
+    }
     
-    
-
+    var nombre = document.getElementById('nombre').value;
+    doc.text( ciudad + ', ' + fechaLarga, 25, 25);
+    doc.text(profesional, 25, 35);
+    doc.setFont('helvetica', 'bold');
+    doc.text(nompro, 25, 45);
+    doc.setFont('helvetica', 'normal');
+    doc.text(cur, 25, 55);
     window.open(doc.output('bloburl'), '_blank');
   }
   return (
@@ -24,6 +42,43 @@ function App() {
       <div class='block text-center pt-10 text-4xl '>
         <label for='fecha'>Fecha:</label>
         <input type='date' id='fecha' name='fecha' onChange={(e) => setFecha(e.target.value)}></input>
+      </div>
+      {/* Ingresar a que ciudad va dirigido de una lista y luego preguntar a cual curaduria de esa ciudad */}
+      <div class='block text-center pt-10 text-4xl '>
+        <label for='ciudad'>Ciudad:</label>
+        <select id='ciudad' name='ciudad'>
+          <option value='Bogotá'>Bogotá</option>
+          <option value='Mosquera'>Mosquera</option>
+          <option value='Funza'>Funza</option>
+          <option value='Madrid'>Madrid</option>
+          <option value='otra'>Otra</option>
+        </select>
+      </div>
+      <div class='block text-center pt-10 text-4xl '>
+        <label for='curaduria'>Curaduría:</label>
+        <select id='curaduria' name='curaduria'>
+          <option value='uno'>Curaduría 1</option>
+          <option value='dos'>Curaduría 2</option>
+          <option value='tres'>Curaduría 3</option>
+          <option value='cuatro'>Curaduría 4</option>
+          <option value='cinco'>Curaduría 5</option>
+        </select>
+      </div>
+      {/* Escribir el nombre de quien da el poder */}
+      <div class='block text-center pt-10 text-4xl '>
+        <label for='nombre'>Nombre:</label>
+        <input type='text' id='nombre' name='nombre'></input>
+      </div>
+      {/* Escoger el tipo de documento de identidad  */}
+      <div class='block text-center pt-10 text-4xl '>
+        <label for='tipo'>Tipo de documento:</label>
+        <select id='tipo' name='tipo'>
+          <option value='cc'>Cédula de ciudadanía</option>
+          <option value='ce'>Cédula de extranjería</option>
+          <option value='ti'>Tarjeta de identidad</option>
+          <option value='pasaporte'>Pasaporte</option>
+          <option value='nit'>NIT</option>
+        </select>
       </div>
 
 
